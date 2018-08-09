da58225fd1de41cf7f90808bfd02691a84ae7142
James Horton <jameshorton@gmail.com>
2018-08-09 12:31:20 -0400

    added message for when no anagrams are found.

--- AnagramsPart1/anagrams.js
+++ AnagramsPart1/anagrams.js
@@ -1,9 +1,10 @@
 function alphabetize(a) {
     return a.toLowerCase().split("").sort().join("").trim();
-   }
+}
 
-   let div1 = document.getElementById('div1')
-function displayOnPage(a){
+let div1 = document.getElementById('div1')
+
+function displayOnPage(a) {
     let text = document.createTextNode(a)
     let span = document.createElement('span')
     span.appendChild(text)
@@ -11,28 +12,32 @@ function displayOnPage(a){
 
 }
 
-function clearDisplay(){
+function clearDisplay() {
     document.getElementById('div1').innerHTML = ""
 }
 
-function displayAnArray(a){
+function displayAnArray(a) {
     clearDisplay()
-    for (i = 0; i < a.length; i++){
+    for (i = 0; i < a.length; i++) {
         displayOnPage(a[i] + " ")
     }
 }
 
-document.getElementById("findButton").onclick = function() {
+document.getElementById("findButton").onclick = function () {
     var typedText = document.getElementById("input").value;
     let anagrams = []
     let alphabetizedTypedText = alphabetize(typedText)
     let numberOfAnagramsFound = 0
-    for (i = 0; i < words.length; i++){
+    for (i = 0; i < words.length; i++) {
         if (alphabetizedTypedText === alphabetize(words[i])) {
             anagrams[numberOfAnagramsFound] = words[i]
             numberOfAnagramsFound++
-        } 
+        }
     }
-    displayAnArray(anagrams)
- }
-
+    if (numberOfAnagramsFound == 0) {
+        clearDisplay()
+        displayOnPage("No anagrams found. Try a different word.")
+    } else {
+        displayAnArray(anagrams)
+    }
+}
