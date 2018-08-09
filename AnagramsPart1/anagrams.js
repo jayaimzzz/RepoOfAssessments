1c327c66221aec104840b95d0974cf64e1fc0030
James Horton <jameshorton@gmail.com>
2018-08-09 12:22:00 -0400

    completed Anagrams part 1

---
+++ AnagramsPart1/anagrams.js
@@ -0,0 +1,38 @@
+function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
+   }
+
+   let div1 = document.getElementById('div1')
+function displayOnPage(a){
+    let text = document.createTextNode(a)
+    let span = document.createElement('span')
+    span.appendChild(text)
+    div1.appendChild(span)
+
+}
+
+function clearDisplay(){
+    document.getElementById('div1').innerHTML = ""
+}
+
+function displayAnArray(a){
+    clearDisplay()
+    for (i = 0; i < a.length; i++){
+        displayOnPage(a[i] + " ")
+    }
+}
+
+document.getElementById("findButton").onclick = function() {
+    var typedText = document.getElementById("input").value;
+    let anagrams = []
+    let alphabetizedTypedText = alphabetize(typedText)
+    let numberOfAnagramsFound = 0
+    for (i = 0; i < words.length; i++){
+        if (alphabetizedTypedText === alphabetize(words[i])) {
+            anagrams[numberOfAnagramsFound] = words[i]
+            numberOfAnagramsFound++
+        } 
+    }
+    displayAnArray(anagrams)
+ }
+
